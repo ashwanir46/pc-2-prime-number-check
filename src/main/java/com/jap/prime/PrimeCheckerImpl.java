fc772c44b828c01f0db0ecb49745d4537c8e8b38
Ashwani Rajput <ashwanir461@gmail.com>
2022-09-05 16:53:46 +0530

    complete code and also done test case

--- src/main/java/com/jap/prime/PrimeCheckerImpl.java
+++ src/main/java/com/jap/prime/PrimeCheckerImpl.java
@@ -1,9 +1,29 @@
 package com.jap.prime;
 
 public class PrimeCheckerImpl {
-    public boolean primeNumberCheck(int number) {
-        // write the logic here to check is a number is prime or not
-        return false;
+    public static void main(String[] args) {
+        PrimeCheckerImpl primeChecker = new PrimeCheckerImpl();
+        System.out.println("primeChecker.primeNumberCheck(7) = " + primeChecker.primeNumberCheck(2));
     }
 
+    public boolean primeNumberCheck(int number) {
+        // write the logic here to check is a number is prime or not
+
+        PrimeChecker primeChecker = number1 -> {
+            int count = 0;
+            if (number == 0) {
+                return false;
+            } else {
+                for (int i = 1; i <= number; i++) {
+                    if (number % i == 0)
+                        count++;
+                }
+                if (count == 2)
+                    return true;
+                else
+                    return false;
+            }
+        };
+        return primeChecker.isPrimeNumber(number);
+    }
 }
